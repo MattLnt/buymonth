13ad4f960e2902dc8a72f5004047995c975ff4b7
MattLnt <lenivet.matiaz@gmail.com>
2026-08-14 16:42:35 -0400

    fix login responsive

--- app/login/page.js
+++ app/login/page.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import CalMark from "@/components/public/CalMark"; // Assurez-vous que le chemin est bon ou ajustez-le
+import CalMark from "../../components/public/CalMark";
 
 export default function LoginPage() {
   const router = useRouter();
